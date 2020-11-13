69bf7424b3ffb0d0289f573ed9fb9244f9b9c0ea
coderus <coderusinbox@gmail.com>
2020-11-13 14:17:27 +0300

    [build] Add aarch64 architecture, push separate images per arch

--- build-all.sh
+++ build-all.sh
@@ -6,3 +6,4 @@ bash build.sh platformsdk.ks i486 $RELEASE
 bash build.sh tooling.ks i486 $RELEASE
 bash build.sh target.ks i486 $RELEASE
 bash build.sh target.ks armv7hl $RELEASE
+bash build.sh target.ks aarch64 $RELEASE
